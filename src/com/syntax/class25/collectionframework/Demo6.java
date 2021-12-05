f233c87a7a6d36bc5880658d3ba9e0ea957dec1e
Asghar Nazir <asghar@syntaxtech.com>
2021-12-05 13:47:21 -0500

    Class 26

--- src/com/syntax/class25/collectionframework/Demo6.java
+++ src/com/syntax/class25/collectionframework/Demo6.java
@@ -4,22 +4,30 @@ import java.util.ArrayList;
 
 public class Demo6 {
     public static void main(String[] args) {
-        ArrayList<String> subjects=new ArrayList<>();
-        subjects.add("SDLC");
-        subjects.add("Manual Testing");
-        subjects.add("Java");
-        subjects.add("Git");
-        //subjects.add("Selenium");
-        subjects.add("TestNg");
-        subjects.add("Cucumber");
-        subjects.add("Sql");
-        subjects.add("APIs");
-        subjects.add("Docker");
-        subjects.add("AWS");
-        subjects.add("Mobile Testing");
-        subjects.add("Mock Interview");
-        System.out.println(subjects);
-        subjects.add(4,"Selenium"); //adds the element after index 4
-        System.out.println(subjects);
+        ArrayList<String> subjects1=new ArrayList<>();
+        subjects1.add("SDLC");
+        subjects1.add("Manual Testing");
+        subjects1.add("Java");
+        subjects1.add("Git");
+        subjects1.add("Selenium");
+        subjects1.add("TestNg");
+        subjects1.add("Cucumber");
+        subjects1.add("Sql");
+        subjects1.add("APIs");
+
+        ArrayList<String> subjects2=new ArrayList<>();
+
+        subjects2.add("TestNg");
+        subjects2.add("Cucumber");
+        subjects2.add("Sql");
+        subjects2.add("APIs");
+        subjects2.add("Docker");
+        subjects2.add("AWS");
+        subjects2.add("Mobile Testing");
+        subjects2.add("Mock Interview");
+        subjects2.retainAll(subjects1);
+        System.out.println(subjects2);
+        // break till 11:45
+
     }
 }
---
+++ src/com/syntax/class26/SetDemo1.java
@@ -0,0 +1,7 @@
+package com.syntax.class26;
+
+public class SetDemo1 {
+    public static void main(String[] args) {
+
+    }
+}
