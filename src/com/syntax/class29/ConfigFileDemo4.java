e65f1cf86c9960cf7e097b01dd177beea17be3d3
Asghar Nazir <asghar@syntaxtech.com>
2021-12-19 15:10:35 -0500

    class 30

--- src/com/syntax/class29/ConfigFileDemo4.java
+++ src/com/syntax/class29/ConfigFileDemo4.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 
 public class ConfigFileDemo4 {
     public static void main(String[] args) throws IOException {
+
+        System.out.println(System.getProperty("file.separator"));
         System.out.println(ConfigReader.read("Files/Trump.properties","UserName"));
     }
 }
